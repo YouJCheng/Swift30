3619d19ef6dc488e1e25e1e4a6e765bd1ccd9ef3
YouJCheng <Yu-J.Cheng@udt.tw>
2017-09-14 16:37:15 +0800

    JSON, Data, UITabBarController
    Make an app to parse Whitehouse petitions using JSON and a tab bar.

---
+++ HackingWithSwiftProject7/HackingWithSwiftProject7/ViewController.swift
@@ -0,0 +1,79 @@
+//
+//  ViewController.swift
+//  HackingWithSwiftProject7
+//
+//  Created by Yu-J.Cheng on 2017/9/14.
+//  Copyright © 2017年 YuChienCheng. All rights reserved.
+//
+
+import UIKit
+import SwiftyJSON
+
+class ViewController: UITableViewController {
+
+    fileprivate lazy var petitions = [[String: String]]()
+    fileprivate lazy var labels = [String: UILabel]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let urlString: String
+
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+        } else {
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
+        }
+
+
+        if let url = URL(string: urlString) {
+            if let data = try? Data(contentsOf: url) {
+                let json = JSON(data: data)
+
+                if json["metadata"]["responseInfo"]["status"].intValue == 200 {
+                    // we're OK to parse!
+                    parse(json: json)
+                    return
+                }
+            }
+        }
+        showError()
+    }
+
+    func parse(json: JSON) {
+        for result in json["results"].arrayValue {
+            let title = result["title"].stringValue
+            let body = result["body"].stringValue
+            let sigs = result["signatureCount"].stringValue
+            let obj = ["title": title, "body": body, "sigs": sigs]
+            petitions.append(obj)
+        }
+        
+        tableView.reloadData()
+    }
+
+    func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let petition = petitions[indexPath.row]
+        cell.textLabel?.text = petition["title"]
+        cell.detailTextLabel?.text = petition["body"]
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.detailItem = petitions[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
+    }
+}
+
---
+++ HackingWithSwiftProject7/Pods/Target Support Files/Pods-HackingWithSwiftProject7/Pods-HackingWithSwiftProject7-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HackingWithSwiftProject7 : NSObject
+@end
+@implementation PodsDummy_Pods_HackingWithSwiftProject7
+@end
