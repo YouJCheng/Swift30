4ad82284461cc177dc0ab1fbefb31ab6c97b36b9
YouJCheng <Yu-J.Cheng@udt.tw>
2017-09-19 14:33:29 +0800

    project27

--- HackingWithSwiftProject21/HackingWithSwiftProject21/ViewController.swift
+++ HackingWithSwiftProject21/HackingWithSwiftProject21/ViewController.swift
@@ -14,25 +14,35 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
 
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Register", style: .plain, target: self, action: #selector(registerLocal))
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Schedule", style: .plain, target: self, action: #selector(scheduleLocal))
     }
 
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
     func registerLocal() {
         let center = UNUserNotificationCenter.current()
-        center.delegate = self
 
-        let show = UNNotificationAction(identifier: "show", title: "Tell me more…", options: .foreground)
-        let category = UNNotificationCategory(identifier: "alarm", actions: [show], intentIdentifiers: [])
-
-        center.setNotificationCategories([category])
-
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+            if granted {
+                print("Yay!")
+            } else {
+                print("D'oh")
+            }
+        }
     }
 
     func scheduleLocal() {
+        registerCategories()
+
         let center = UNUserNotificationCenter.current()
+
+        // not required, but useful for testing!
+        center.removeAllPendingNotificationRequests()
 
         let content = UNMutableNotificationContent()
         content.title = "Late wake up call"
@@ -50,6 +60,15 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
         center.add(request)
     }
 
+    func registerCategories() {
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+
+        let show = UNNotificationAction(identifier: "show", title: "Tell me more…", options: .foreground)
+        let category = UNNotificationCategory(identifier: "alarm", actions: [show], intentIdentifiers: [])
+
+        center.setNotificationCategories([category])
+    }
 
     func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
         // pull out the buried userInfo dictionary
@@ -57,23 +76,22 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
 
         if let customData = userInfo["customData"] as? String {
             print("Custom data received: \(customData)")
-
+            
             switch response.actionIdentifier {
             case UNNotificationDefaultActionIdentifier:
-                // the user swiped to unlock
+                // the user swiped to unlock; do nothing
                 print("Default identifier")
-
+                
             case "show":
-                // the user tapped our "show more info…" button
                 print("Show more information…")
                 break
-
+                
             default:
                 break
             }
         }
         
-        // you must call the completion handler when you're done
+        // you need to call the completion handler when you're done
         completionHandler()
     }
 
---
+++ HackingWithSwiftProject25/HackingWithSwiftProject25/ViewController.swift
@@ -0,0 +1,150 @@
+//
+//  ViewController.swift
+//  HackingWithSwiftProject25
+//
+//  Created by Yu-J.Cheng on 2017/9/19.
+//  Copyright © 2017年 YuChienCheng. All rights reserved.
+//
+
+import UIKit
+import MultipeerConnectivity
+
+
+class ViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, MCSessionDelegate, MCBrowserViewControllerDelegate  {
+
+    fileprivate var images = [UIImage]()
+    fileprivate var peerID: MCPeerID!
+    fileprivate var mcSession: MCSession!
+    fileprivate var mcAdvertiserAssistant: MCAdvertiserAssistant!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Selfie Share"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(importPicture))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(showConnectionPrompt))
+
+        peerID = MCPeerID(displayName: UIDevice.current.name)
+        mcSession = MCSession(peer: peerID, securityIdentity: nil, encryptionPreference: .required)
+        mcSession.delegate = self
+
+
+    }
+
+    func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+
+    func showConnectionPrompt() {
+        let ac = UIAlertController(title: "Connect to others", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "Host a session", style: .default, handler: startHosting))
+        ac.addAction(UIAlertAction(title: "Join a session", style: .default, handler: joinSession))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
+    }
+
+    func startHosting(action: UIAlertAction) {
+        mcAdvertiserAssistant = MCAdvertiserAssistant(serviceType: "hws-project25", discoveryInfo: nil, session: mcSession)
+        mcAdvertiserAssistant.start()
+    }
+
+    func joinSession(action: UIAlertAction) {
+        let mcBrowser = MCBrowserViewController(serviceType: "hws-project25", session: mcSession)
+        mcBrowser.delegate = self
+        present(mcBrowser, animated: true)
+    }
+
+
+
+    // ImagePickerController Delegate
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        guard let image = info[UIImagePickerControllerEditedImage] as? UIImage else { return }
+
+
+        dismiss(animated: true)
+
+        images.insert(image, at: 0)
+        collectionView?.reloadData()
+
+        // 1
+        if mcSession.connectedPeers.count > 0 {
+            // 2
+            if let imageData = UIImagePNGRepresentation(image) {
+                // 3
+                do {
+                    try mcSession.send(imageData, toPeers: mcSession.connectedPeers, with: .reliable)
+                } catch {
+                    let ac = UIAlertController(title: "Send error", message: error.localizedDescription, preferredStyle: .alert)
+                    ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                    present(ac, animated: true)
+                }
+            }
+        }
+    }
+
+    // MARK: CollectionView Data Source
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageView", for: indexPath)
+        if let imageView = cell.viewWithTag(1000) as? UIImageView {
+            imageView.image = images[indexPath.item]
+        }
+        
+        return cell
+    }
+
+
+    // MARK: MCSessionDelegate
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+
+
+    }
+
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+
+    }
+
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL, withError error: Error?) {
+        
+    }
+
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        if let image = UIImage(data: data) {
+            DispatchQueue.main.async { [unowned self] in
+                self.images.insert(image, at: 0)
+                self.collectionView?.reloadData()
+            }
+        }
+    }
+
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        switch state {
+        case MCSessionState.connected:
+            print("Connected: \(peerID.displayName)")
+
+        case MCSessionState.connecting:
+            print("Connecting: \(peerID.displayName)")
+
+        case MCSessionState.notConnected:
+            print("Not Connected: \(peerID.displayName)")
+        }
+    }
+
+    //MARK: MCBrowserViewControllerDelegate
+    func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
+        dismiss(animated: true)
+    }
+
+    func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
+        dismiss(animated: true)
+    }
+
+    
+
+}
+
