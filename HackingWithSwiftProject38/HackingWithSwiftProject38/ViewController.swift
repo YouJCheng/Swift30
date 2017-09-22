8ec2ca7081fbb6f08744639ebf25b92ab122a03b
YouJCheng <Yu-J.Cheng@udt.tw>
2017-09-22 09:17:48 +0800

    test

---
+++ HackingWithSwiftProject38/HackingWithSwiftProject38/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HackingWithSwiftProject38
+//
+//  Created by Yu-J.Cheng on 2017/9/21.
+//  Copyright © 2017年 YuChienCheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
